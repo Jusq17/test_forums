35b4791f44f5eb06dc664e704b433925cd33f930
Jusq17 <86538024+Jusq17@users.noreply.github.com>
2021-06-28 04:44:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.secret_key = "salattu"
-app.config["SQLALCHEMY_DATABASE_URI"] = "salattu"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://jtrbebwzyskmln:53f8e7c7e4f65fe9ca30ff1741dccfc1d95f46aaeb761b1c38f0a19217d64e1a@ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/dcbo0o6er550eq"
 
 
 db = SQLAlchemy(app)
