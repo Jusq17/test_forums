da9a57e25d70c9844bfbf9b2c2206b8fc18907d4
Jusq17 <86538024+Jusq17@users.noreply.github.com>
2021-07-01 04:45:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -277,6 +277,7 @@ def edit_secret_message(id_num):
 
 @app.route("/edit_comment/<int:id_num>") 
 def edit_comment(id_num):
+    username = session["username"]
     sql = "SELECT 1 FROM comments WHERE id =:id_num and username =:username"
     result = db.session.execute(sql,{"id_num":id_num, "username":username})
     rights = result.fetchone()
