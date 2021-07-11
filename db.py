12149b58aea4e71a607a91a32f12ebb33036ff3e
Jusq17 <86538024+Jusq17@users.noreply.github.com>
2021-07-11 04:27:31 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://jtrbebwzyskmln:53f8e7c7e4f65fe9ca30ff1741dccfc1d95f46aaeb761b1c38f0a19217d64e1a@ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/dcbo0o6er550eq"
+app.config["SQLALCHEMY_DATABASE_URI"] = "salattu"
 db = SQLAlchemy(app)
